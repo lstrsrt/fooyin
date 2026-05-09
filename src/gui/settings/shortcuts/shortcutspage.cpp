9e3545fa2f97b2865abd7a28efa5d731074f88b4
Luke Taylor <luket@pm.me>
2026-05-09 02:01:51 +0100

    [gui] ShortcutsPage: Add filter to search shortcuts

--- src/gui/settings/shortcuts/shortcutspage.cpp
+++ src/gui/settings/shortcuts/shortcutspage.cpp
@@ -33,12 +33,78 @@
 #include <QKeyEvent>
 #include <QKeySequenceEdit>
 #include <QLabel>
+#include <QLineEdit>
 #include <QPushButton>
+#include <QRegularExpression>
+#include <QSortFilterProxyModel>
 #include <QTreeView>
 
 using namespace Qt::StringLiterals;
 
 namespace Fooyin {
+class ShortcutsFilterModel : public QSortFilterProxyModel
+{
+    Q_OBJECT
+
+public:
+    using QSortFilterProxyModel::QSortFilterProxyModel;
+
+protected:
+    [[nodiscard]] bool filterAcceptsRow(int sourceRow, const QModelIndex& sourceParent) const override
+    {
+        if(filterRegularExpression().pattern().isEmpty()) {
+            return true;
+        }
+
+        const QModelIndex sourceIndex = sourceModel()->index(sourceRow, 0, sourceParent);
+        if(matchesRow(sourceIndex) || ancestorMatches(sourceParent)) {
+            return true;
+        }
+
+        const int rowCount = sourceModel()->rowCount(sourceIndex);
+        for(int row{0}; row < rowCount; ++row) {
+            if(filterAcceptsRow(row, sourceIndex)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+private:
+    [[nodiscard]] bool matchesRow(const QModelIndex& sourceIndex) const
+    {
+        if(!sourceIndex.isValid()) {
+            return false;
+        }
+
+        const auto expression = filterRegularExpression();
+
+        const int columnCount = sourceModel()->columnCount(sourceIndex.parent());
+        for(int column{0}; column < columnCount; ++column) {
+            const QModelIndex columnIndex = sourceIndex.siblingAtColumn(column);
+
+            if(sourceModel()->data(columnIndex, Qt::DisplayRole).toString().contains(expression)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    [[nodiscard]] bool ancestorMatches(QModelIndex sourceParent) const
+    {
+        while(sourceParent.isValid()) {
+            if(matchesRow(sourceParent)) {
+                return true;
+            }
+            sourceParent = sourceParent.parent();
+        }
+
+        return false;
+    }
+};
+
 class ShortcutInput : public ExpandableInput
 {
     Q_OBJECT
@@ -97,8 +163,10 @@ private:
 
     ActionManager* m_actionManager;
 
+    QLineEdit* m_filter;
     QTreeView* m_shortcutTable;
     ShortcutsModel* m_model;
+    ShortcutsFilterModel* m_proxyModel;
     QGroupBox* m_shortcutBox;
     ExpandableInputBox* m_inputBox;
     QLabel* m_conflictLabel;
@@ -107,22 +175,32 @@ private:
 
 ShortcutsPageWidget::ShortcutsPageWidget(ActionManager* actionManager)
     : m_actionManager{actionManager}
+    , m_filter{new QLineEdit(this)}
     , m_shortcutTable{new QTreeView(this)}
     , m_model{new ShortcutsModel(this)}
+    , m_proxyModel{new ShortcutsFilterModel(this)}
     , m_shortcutBox{new QGroupBox(this)}
     , m_inputBox{new ExpandableInputBox(tr("Shortcuts"), ExpandableInput::ClearButton | ExpandableInput::CustomWidget,
                                         this)}
     , m_conflictLabel{new QLabel(this)}
     , m_reassignButton{new QPushButton(tr("Overwrite Shortcut"), this)}
 {
-    m_shortcutTable->setModel(m_model);
+    m_filter->setPlaceholderText(tr("Filter shortcuts"));
+    m_filter->setClearButtonEnabled(true);
+
+    m_proxyModel->setSourceModel(m_model);
+    m_proxyModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
+
+    m_shortcutTable->setModel(m_proxyModel);
     m_shortcutTable->hideColumn(1);
     m_shortcutTable->header()->setSectionResizeMode(QHeaderView::ResizeToContents);
 
     auto* layout = new QGridLayout(this);
     layout->setContentsMargins(0, 0, 0, 0);
 
-    layout->addWidget(m_shortcutTable, 0, 0);
+    layout->addWidget(m_filter, 0, 0);
+    layout->addWidget(m_shortcutTable, 1, 0);
+    layout->setRowStretch(1, 1);
 
     QObject::connect(m_model, &QAbstractItemModel::modelReset, m_shortcutTable, &QTreeView::expandAll);
 
@@ -146,13 +224,19 @@ ShortcutsPageWidget::ShortcutsPageWidget(ActionManager* actionManager)
     m_reassignButton->hide();
     groupLayout->addWidget(m_reassignButton, 0, Qt::AlignLeft);
 
-    layout->addWidget(m_shortcutBox, 1, 0);
+    layout->addWidget(m_shortcutBox, 2, 0);
 
     QObject::connect(resetShortcut, &QAbstractButton::clicked, this, &ShortcutsPageWidget::resetCurrentShortcut);
     QObject::connect(m_reassignButton, &QAbstractButton::clicked, this, &ShortcutsPageWidget::reassignConflicts);
     QObject::connect(m_shortcutTable->selectionModel(), &QItemSelectionModel::selectionChanged, this,
                      &ShortcutsPageWidget::selectionChanged);
     QObject::connect(m_inputBox, &ExpandableInputBox::blockDeleted, this, &ShortcutsPageWidget::shortcutDeleted);
+    QObject::connect(m_filter, &QLineEdit::textChanged, this, [this](const QString& text) {
+        m_proxyModel->setFilterRegularExpression(
+            QRegularExpression{QRegularExpression::escape(text), QRegularExpression::CaseInsensitiveOption});
+        m_shortcutTable->expandAll();
+        selectionChanged();
+    });
 
     m_shortcutBox->setDisabled(true);
 }
@@ -188,7 +272,7 @@ Command* ShortcutsPageWidget::selectedCommand() const
         return nullptr;
     }
 
-    const QModelIndex index = selected.front();
+    const QModelIndex index = m_proxyModel->mapToSource(selected.front());
     if(index.data(ShortcutItem::IsCategory).toBool()) {
         return nullptr;
     }
